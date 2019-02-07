e2ac3b512f996da5bb59ea67353b521cbb40c966
Alumno <ulsa@iMacD107.local>
2019-02-06 18:30:57 -0700

    Style 1

--- resources/views/mascotas/index.blade.php
+++ resources/views/mascotas/index.blade.php
@@ -1,33 +1,46 @@
 @extends('layouts.default')
+@section('title', 'Mascotas - Lista de Mascotas')
+@section('titulo', 'Mascotas')
+@section('subtitulo', 'Lista de mascotas')
 @section('contenido')
-<a href="{{route('mascotas.create')}}">
-    <button>Agregar mascota</button>
-</a>
-<table>
-    <thead>
-        <tr>
-            <th>Nombre</th>
-            <th>Precio</th>
-            <th>Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($mascotas as $mascota)
-            <tr>
-                <td>{{ $mascota->Nombre }}</td>
-                <td>{{ $mascota->Precio }}</td>
-                <td>
-                    <a href="{{route('mascotas.edit', $mascota->ID)}}">
-                        <button>Editar</button>
-                    </a>
-                    <form method="POST" action="{{ route('mascotas.destroy', $mascota->ID) }}">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit">Borrar</button>
-                    </form>                        
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title"></h3>
+                <a href="{{route('mascotas.create')}}">
+                    <button class="btn btn-primary">Agregar mascota</button>
+                </a>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Precio</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($mascotas as $mascota)
+                            <tr>
+                                <td>{{ $mascota->Nombre }}</td>
+                                <td>$ {{ $mascota->Precio }}.00</td>
+                                <td>
+                                    <a class="btn btn-primary" href="{{route('mascotas.edit', $mascota->ID)}}">
+                                        <i class="fa fa-fw fa-edit"></i>
+                                    </a>
+                                    <form method="POST" action="{{ route('mascotas.destroy', $mascota->ID) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger" type="submit"><i class="fa fa-fw fa-trash"></i></button>
+                                    </form>                        
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
