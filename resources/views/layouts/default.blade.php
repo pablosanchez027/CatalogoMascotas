6452327d723ef47bdca039d3b1cf57e081fb25a0
Alumno <ulsa@iMacD107.local>
2019-02-27 17:07:13 -0700

    year - blade

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -202,7 +202,7 @@ desired effect
       Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2019 <a href="/#">Company</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; {{ now()->year }} <a href="/#">Company</a>.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
