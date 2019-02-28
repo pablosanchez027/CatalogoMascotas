4d04e09ad71c58215053f4bc0586d3113094d7eb
Alumno <ulsa@iMacD107.local>
2019-02-27 17:48:43 -0700

    Perfil Edit

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class PerfilController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function edit() {
+        $usuario = Auth::user();
+        
         $argumentos = array();
-        return view('perfil.edit');
+        $argumentos['usuario'] = $usuario;
+
+        return view('perfil.edit', $argumentos);
     }
 }
--- resources/views/perfil/edit.blade.php
+++ resources/views/perfil/edit.blade.php
@@ -11,9 +11,37 @@
             </div>
             <div class="box-body">
                 Formulario
-                
+                <div class="form-group">
+                    <label>Nombre</label>
+                    <input class="form-control" value="{{ $usuario->name }}" type="text">
+                </div>
+                <div class="form-group">
+                    <label>Correo</label>
+                    <input class="form-control" type="email" value="{{ $usuario->email }}" readonly>
+                </div>
+                <div class="form-group">
+                    <label>Foto</label>
+                    <input class="form-control" type="file">
+                </div>
+                <div class="form-group">
+                    @if($usuario->foto)
+                    <img src="{{ $usuario->foto }}" style="width: 400px; height: auto;" class="img-responsive">
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label>Contraseña</label>
+                    <input class="form-control" type="password">
+                </div>
+                <div class="form-group">
+                    <label>Confirmar Contraseña</label>
+                    <input class="form-control" type="password">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+                </div>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
